ff80cc3f6c724dcd706e6d14039e474cd6bb051e
xiaokai.zhan <zhanxiaokai@changba.com>
2017-07-11 12:01:41 +0800

    Initial Commit

---
+++ video_playerTests/video_playerTests.m
@@ -0,0 +1,39 @@
+//
+//  video_playerTests.m
+//  video_playerTests
+//
+//  Created by apple on 2017/7/11.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface video_playerTests : XCTestCase
+
+@end
+
+@implementation video_playerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
